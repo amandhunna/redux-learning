46d5add8c95e697211228a4d5a8cc2bd35c0ccdd
Amanjot Singh <amandhunna.dev@gmail.com>
2021-11-08 14:58:42 +0530

    added basic store in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const initialState = {
+  count: 0,
+}
+
+const actions = {
+  inc: "INCREMENT"
+}
+
+const reducer = (state = initialState, action) => {
+  const { type } = action;
+  if(type === actions.inc) {
+    return {
+      count: state.count + 1,
+    };
+  }
+
+  return state;
+}
+
+const store = createStore(reducer);
+
+
+function Counter() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         count: 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          <button>Inc</button>
+          <button>Dec</button>
+          <button>Reset</button>
+        </section>
       </header>
     </div>
   );
 }
 
-export default App;
+export default Counter;
