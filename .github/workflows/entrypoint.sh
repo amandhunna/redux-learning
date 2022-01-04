3ad3f044cfd5049ede71897d7d17b1728658315f
amandhunna <amandhunna.dev@gmail.com>
2022-01-05 05:14:29 +0530

    fixed new file update

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh -l
 
-echo "Hello $current"
-time=$(date)
-echo ":ne:set-output name=time::$time"
+mainBuildSize=$(jq '.[0].mainBuildSize' ./buildSize.json)
+#Snpm run build
+currentBuildSize=$(du -s ./build | cut -f1)
+difference=`expr ${mainBuildSize} - ${currentBuildSize}`
+echo "current:$currentBuildSize, mainBuildSize=${mainBuildSize}, difference: ${difference}"
 
-# current=$(du -sh ./)
+contents="this is working"
+#contt=`$(jq '.[0].prevBuildSize="${mainBuildSize}" .[0].mainBuildSize="${currentBuildSize}" .[0].difference="${difference}"' buildSize.json)`
+
+echo "pre" $(jq . buildSize.json)
+
+up=$(jq --arg var1 "$currentBuildSize" '.[0].mainBuildSize=$var1' buildSize.json)
+echo ${up} > test.json
