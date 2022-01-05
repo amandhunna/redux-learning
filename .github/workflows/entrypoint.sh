9b5b9bfebe0ea87bbe48b8c055dd0df576359b3c
amandhunna <amandhunna.dev@gmail.com>
2022-01-05 10:28:45 +0530

    added new workflow

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
-mainBuildSize=$(jq '.[0].mainBuildSize' ./buildSize.json)
+declare -i mainBuildSize=$(jq '.[0].mainBuildSize' ./buildSize.json)
 #npm run build
-currentBuildSize=$(du -s ./build | cut -f1)
-difference=`expr ${mainBuildSize} - ${currentBuildSize}`
-#echo "[{ current:$currentBuildSize, mainBuildSize:${mainBuildSize}, difference: ${difference} }]" 
+declare -i currentBuildSize=$(du -s ./build | cut -f1)
+difference=`expr ${currentBuildSize} - ${mainBuildSize}`
+echo "[{ current:$currentBuildSize, mainBuildSize:${mainBuildSize}, difference: ${difference} }]" 
 
-up=$(jq --arg var1  "$currentBuildSize" --arg var2  "$mainBuildSize" --arg var3  "$difference" '.[0].mainBuildSize=$var1 | .[0].difference=$var3 | .[0].prevBuildSize=$var2' buildSize.json)
+up=$(jq --arg var1 $currentBuildSize --arg var2 $mainBuildSize --arg var3  $difference '.[0].mainBuildSize=$var1 | .[0].difference=$var3 | .[0].prevBuildSize=$var2' buildSize.json)
 echo ${up} > buildSize.json
