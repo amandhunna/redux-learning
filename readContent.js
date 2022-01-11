7e63a31782156332bc18cff26b1aa44a51b583e3
amandhunna <amandhunna.dev@gmail.com>
2022-01-11 23:34:48 +0530

    test, added base build file and removed current build size

--- readContent.js
+++ readContent.js
@@ -1,29 +1,31 @@
 
 const path = require('path');
 const fs = require('fs');
-const { error } = require('console');
 
-const baseFile = path.join(__dirname, 'base.txt')
-const currentFile = path.join(__dirname, 'current.txt')
+const baseFile = path.join(__dirname, 'masterBuild.txt');
 
 const fileTracker = {/* filepath: { base, current } */};
-console.log("-------running-----------------")
+console.log("-------running-----------------");
+
+console.log("----", __dirname);
+
 /* reading base */
 fs.readFile(baseFile, 'utf8', function(error, baseData){      
     if(error) {
         console.error("error is base: ", error)
     } else {
-        
-        /* reading current */
-        fs.readFile(currentFile, 'utf8', function(error, currentData) {      
+        addProperties(baseData,"base", fileTracker)
+        // reading current * /
+/*         fs.readFile(currentFile, 'utf8', function(error, currentData) {      
             if(error) {
                 console.error("error in currentData: ", error)
             } else {
-                addProperties(baseData,"base", fileTracker)
                 addProperties(currentData, "current", fileTracker)
             }
             console.log(fileTracker);
         });
+ */
+        console.log(fileTracker);
     }
 });
 
