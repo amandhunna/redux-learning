18bb3b3529f7848dce3d5c7d3e5a9d0b3ebac5c9
amandhunna <amandhunna.dev@gmail.com>
2022-01-12 00:22:25 +0530

    test, removed master job and checking spawn

--- readContent.js
+++ readContent.js
@@ -2,7 +2,19 @@
 const path = require('path');
 const fs = require('fs');
 const { spawn } = require("child_process");
+const ls = spawn('ls', ['-lh', '/usr']);
 
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
 
 
 const baseFile = path.join(__dirname, 'masterBuild.txt');
