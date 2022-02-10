5361df2d39273b82c3ed0f1cf1629956457add81
amandhunna <amandhunna.dev@gmail.com>
2022-02-10 18:09:40 +0530

    base setup for PR comment,1

--- readContent.js
+++ readContent.js
@@ -2,59 +2,61 @@
 const path = require('path');
 const fs = require('fs');
 const core = require('@actions/core');
+const github = require('@actions/github');
 
-const args =  process.argv;
-const fileTracker = {/* filepath: { base, current } */};
+console.log("github", github);
 
-/* adding current build files size to fileTracker */
-for(let index=2; index<=args.length -2; index+=2) {
-    const filePath = args[index+1];
-    const size = args[index];
-    fileTracker[filePath] =  { base: 0, current: size }
-};
+function runBuild() {
+    const args =  process.argv;
+    const fileTracker = {/* filepath: { base, current } */};
 
-/* reading base */
-const baseFile = path.join(__dirname, 'masterBuild.txt');
+    /* adding current build files size to fileTracker */
+    for(let index=2; index<=args.length -2; index+=2) {
+        const filePath = args[index+1];
+        const size = args[index];
+        fileTracker[filePath] =  { base: 0, current: size }
+    };
 
-/* check if base file exits */
-const isBaseFileExist = fs.existsSync(baseFile);
+    /* reading base */
+    const baseFile = path.join(__dirname, 'masterBuild.txt');
 
-try {
-    if (isBaseFileExist) {
-        core.info("isBaseFileExist exsist");
-        fs.readFile(baseFile, 'utf8', function(error, baseData){      
-            if(error) {
-                // console.error("error is base: ", error.stack)
-                core.setFailed(`Action failed with error :: ${error}`);
-            } else {
-                addProperties(baseData,"base", fileTracker);
-        
-                core.info('Output: build report');
-                //core.info(JSON.parse(fileTracker))
-                console.log(fileTracker);
-            }
-        });
-    } else {
-        core.info('Output: build report: base file does not exsist');
-        //core.info(JSON.stringify(fileTracker))
-        console.log(fileTracker);
-    }
-  } catch(error) {
-    core.setFailed(`Action failed with error :: ${error}`);
-  }
-
-
-
-/* util */
-function addProperties(dataLines,type, fileTracker) {
-    const lines = dataLines.split("\n");
-    lines.forEach(line => {
-        const [size, filePath] = line.split("\t");
-        if(![undefined, 'undefined'].includes(filePath)) {
-            if(!Object.keys(fileTracker).includes(filePath)) {
-                fileTracker[filePath] = { base: 0, current: 0 };
+    /* check if base file exits */
+    const isBaseFileExist = fs.existsSync(baseFile);
+    /* util */
+    function addProperties(dataLines,type, fileTracker) {
+        const lines = dataLines.split("\n");
+        lines.forEach(line => {
+            const [size, filePath] = line.split("\t");
+            if(![undefined, 'undefined'].includes(filePath)) {
+                if(!Object.keys(fileTracker).includes(filePath)) {
+                    fileTracker[filePath] = { base: 0, current: 0 };
+                };
+                fileTracker[filePath][type] =  size;
             };
-            fileTracker[filePath][type] =  size;
-        };
-    });
-};
+        });
+    };
+
+    try {
+      if (isBaseFileExist) {
+          core.info("isBaseFileExist exsist");
+          fs.readFile(baseFile, 'utf8', function(error, baseData){      
+              if(error) {
+                  // console.error("error is base: ", error.stack)
+                  core.setFailed(`Action failed with error :: ${error}`);
+              } else {
+                  addProperties(baseData,"base", fileTracker);
+                
+                  core.info('Output: build report');
+                  //core.info(JSON.parse(fileTracker))
+                  console.log(fileTracker);
+              }
+          });
+      } else {
+          core.info('Output: build report: base file does not exsist');
+          //core.info(JSON.stringify(fileTracker))
+          console.log(fileTracker);
+      }
+    } catch(error) {
+      core.setFailed(`Action failed with error :: ${error}`);
+    }
+}
