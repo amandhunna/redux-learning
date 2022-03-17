1769577cbc84975ab359107f60a8cc5c8e2d1728
amandhunna <amandhunna.dev@gmail.com>
2022-03-17 17:16:38 +0530

    label: check label after commenting git push

--- console.js
+++ console.js
@@ -3,5 +3,5 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 // console.log("---core----", core);
-const x = github.context.payload.pull_request
+const x = github.context.payload.pull_request.labels
 console.log("----github",Object.keys(x), x)
