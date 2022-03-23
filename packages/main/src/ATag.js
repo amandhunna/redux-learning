8db90473c2324268b866fadc4b9d67dca91c34a8
amandhunna <amandhunna.dev@gmail.com>
2022-03-23 14:23:14 +0530

    route setup

---
+++ packages/main/src/ATag.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+
+export default function ATag() {
+    console.log("y m loaded");
+  return (
+    <div><Link to="/">back</Link></div>
+  )
+}
--- packages/main/src/App.js
+++ packages/main/src/App.js
@@ -3,23 +3,35 @@ import logo from './logo.svg';
 import MainContext from "./Context";
 import Anchor from "./Anchor"
 import { formatDate } from "@aman/utils";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+
+import ATag from "./ATag";
 
 function App() {
   return (
-    <MainContext.Provider value={{ contextType: "updated" }}>
-      <LibContext.Provider value={{contextType: "updated"}}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              {formatDate()}
-            </p>
-            <Anchor />
-          </header>
-        </div>
-      </LibContext.Provider>
-    </MainContext.Provider>
+    <Routes>
+      <Route path="/" element={
+        <MainContext.Provider value={{ contextType: "updated" }}>
+          <LibContext.Provider value={{contextType: "updated"}}>
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  {formatDate()}{console.log("---loaded-/")}
+                </p>
+                <Anchor />
+              </header>
+            </div>
+          </LibContext.Provider>
+        </MainContext.Provider>} />
+        <Route path="/c" element={
+          <div style={{
+            height: "100vh", 
+            width: "100vw",
+            background: "red"
+          }}> {formatDate()}{console.log("-im loaded but not ATag-")}<ATag/></div>} />
+    </Routes>
   );
 }
 
