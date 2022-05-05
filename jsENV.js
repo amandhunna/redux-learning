ba06b11e9cd2d915b52a7b45aede3e023d2073e1
amandhunna <amandhunna.dev@gmail.com>
2022-05-05 21:06:30 +0530

    feat: updated overlay value

--- jsENV.js
+++ jsENV.js
@@ -1,6 +1,6 @@
 const resolvePercyTokens = () => {
     fs = require("fs");
-    fs.writeFile("temptokens.txt", "[portal-mweb, portal-dweb]", function (err) {
+    fs.writeFile("temptokens.txt", "[portal-mweb]", function (err) {
       if (err) return console.log(err);
       console.log("Temp Tokens > temptokens.txt");
     });
