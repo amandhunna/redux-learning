8d4ef7fc60bc98f53e6405dfaec5c89c02f6db82
amandhunna <amandhunna.dev@gmail.com>
2022-05-18 03:53:49 +0530

    implement route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { actions } from './store';
 import { connect } from 'react-redux';
 import { fetchProduct } from './action';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App(props) {
@@ -16,17 +22,20 @@ function App(props) {
     dispatch(fetchProduct());
   }
 
-  const renderProducts = () => {
-    if(!count) {
-      return "NO PRODUCT FETCHED";
+  const Product = ({id}) => {
+    if(!id) {
+      return "NO PRODUCT Found";
     }
-    const productList = Object.entries(product[count]);
-
-    
+    const productList = Object.entries(product[id]);
     return productList.map(([key, value]) => {
       const liKey = `${key}${value}`
       return <li key={liKey}><span>{key}</span>:<strong>{value}</strong></li>
     });
+  }
+
+  const ProductList = () => {
+    const items = Object.values(product);
+    return items.map(({id}) => <Product key={`product-${id}`} id={id} />)
   }
 
   const pageCount = () => {
@@ -36,20 +45,43 @@ function App(props) {
 
   const style = { ...product[count]?.color ? { backgroundColor: product[count].color} : {} };
 
+
+  const Home = () => (
+    <header className="App-header" style={style}>
+    <p>
+     Product id: {pageCount()}
+    </p>
+    <p>
+     <Product id={count}/>
+    </p>
+    <section>
+      <button onClick={onResetClick}>Reset</button>
+      <button onClick={onFetchProduct}>Next Product</button>
+    </section>
+    <Link to="/all">All products</Link>
+    </header>
+  );
+  const AllProducts = () => (
+    <section>
+      <span>All fetch products</span>
+      <p>
+        <ProductList />
+      </p>
+      <Link to="/">Home</Link>
+    </section>);
+
+  const NoPage =() => <section>No page found. <Link to="/">Home</Link> <Link to="/all">All products</Link></section>
+
+
   return (
      <div className="App">
-       <header className="App-header" style={style}>
-         <p>
-          Product id: {pageCount()}
-         </p>
-         <p>
-          {renderProducts()}
-         </p>
-         <section>
-           <button onClick={onResetClick}>Reset</button>
-           <button onClick={onFetchProduct}>Next Product</button>
-         </section>
-       </header>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="all" element={<AllProducts />} />
+      <Route path="*" element={<NoPage />} />
+    </Routes>
+  </BrowserRouter>
      </div>
     
   );
--- src/action.js
+++ src/action.js
@@ -8,7 +8,7 @@ export const getProduct = data => ({
 export const fetchProduct = () => {
     return (dispatch, getState) => {
         const page = getState().count + 1;
-        console.log("here",  getState().count, page)
+        // console.log("here",  getState().count, page)
         fetch(`https://reqres.in/api/products/${page}`)
         .then((res) => res.json())
         .then(response => { 
